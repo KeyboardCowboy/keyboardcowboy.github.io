b6de80a8b8664fe396deac7efb197fa6a4a5128e
KeyboardCowboy <chris@162unlimited.com>
2015-04-27 00:23:09 -0600

    Summary config.

--- _js/chris.js
+++ _js/chris.js
@@ -2,14 +2,31 @@
   var Chris = {
     // Init
     init: function() {
-      $("#logo a").click(function(event) {
+      this.menu = $("#under .inner");
+    },
+
+    // Bind menu toggle actions
+    toggleMenu: function() {
+      // Bind toggler to the main button
+      $("a.js-menu-toggle").on('click', function(event) {
         event.preventDefault();
         $("body").toggleClass('open');
+      });
+
+      // Hide menu when link is clicked
+      $("ul.posts a", this.menu).on('click', function(event) {
+        var link = $(this);
+        event.preventDefault();
+        $("body").removeClass('open');
+        var timer = setTimeout(function() {
+          location.href = link.attr('href');
+        }, 500);
       });
     }
   };
 
   $(document).ready(function() {
     Chris.init();
+    Chris.toggleMenu();
   });
 })(jQuery);
